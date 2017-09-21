f8638b49e4b6b793bb4bc09370684ef1ce5090ae
Baku <bakumn@users.noreply.github.com>
2017-09-21 15:38:48 +0800

    tutorial done
    tutorial done https://www.howtographql.com/

--- src/index.js
+++ src/index.js
@@ -5,26 +5,39 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
-import { GC_AUTH_TOKEN } from './constants'
+import { GC_AUTH_TOKEN } from './constants';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
 const networkInterface = createNetworkInterface({
     uri: 'https://api.graph.cool/simple/v1/cj7sty6ug0dn4014600egnkjs'
 });
 
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj7sty6ug0dn4014600egnkjs', {
+    reconnect: true,
+    connectionParams: {
+       authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    }
+})
+  
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+    networkInterface,
+    wsClient
+)
+  
 networkInterface.use([{
     applyMiddleware(req, next) {
-        if (!req.options.headers) {
-            req.options.headers = {}
-        }
-        const token = localStorage.getItem(GC_AUTH_TOKEN)
-        req.options.headers.authorization = token ? `Bearer ${token}` : null
-        next()
+      if (!req.options.headers) {
+        req.options.headers = {}
+      }
+      const token = localStorage.getItem(GC_AUTH_TOKEN)
+      req.options.headers.authorization = token ? `Bearer ${token}` : null
+      next()
     }
-}]);
-
+}])
+  
 const client = new ApolloClient({
-    networkInterface
-});
+    networkInterface: networkInterfaceWithSubscriptions
+})
 
 ReactDOM.render(
     <BrowserRouter>
